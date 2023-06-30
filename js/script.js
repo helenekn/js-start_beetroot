5ea248d5dbff52ee909ea29efd9423e812fdab42
Helen <redkina2508@gmail.com>
2023-06-30 22:23:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let userName = prompt("Як Вас звати?", "");
+let userCountry = prompt("Ваша країна", "");
+let nameAcademy = prompt("Введіть назву академії, де Ви навчаєтесь", "");
+let nameCourse = prompt("Введіть назву курсу", "");
+let developerSalary = prompt("Яка ваша бажана зарплата?");
+
+console.log(
+   `Шановний ${userName} з чудової країни ${userCountry}! Після закінчення курсу "${nameCourse}" академії "${nameAcademy}" ви стовідсотково знайдете роботу із зарплатою ${developerSalary} та ментора у подарунок) `
+);
